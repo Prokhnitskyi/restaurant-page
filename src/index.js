b28b5e7557ac64b7c729777894f7b96c598eeee2
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-10 20:53:19 +0300

    Add homepage (static in the template)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles/style.scss';
+
+// temp
+import heroImg from './assets/images/hero.webp';
+import logoImg from './assets/images/logo.svg';
+document.querySelector('.hero__image').src = heroImg;
+document.querySelector('.header__logo-img').src = logoImg;
