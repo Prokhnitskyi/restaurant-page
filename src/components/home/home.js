a43d8b9dcd493cb53d6e320cd22f297ff4037a99
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-11 18:50:51 +0300

    Add home page content

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,3 +1,24 @@
 // temp
+import './home.scss';
 import heroImg from '../../assets/images/hero.webp';
-document.querySelector('.hero__image').src = heroImg;
+import texts from '../../assets/data/text-resourses.json';
+
+export function getHome() {
+  return `
+    <section class="hero">
+        <h2 class="hero__title">${texts.hero.name}</h2>
+        <img src="${heroImg}" alt="Hero image" class="hero__image">
+    </section>
+    <section class="about">
+        ${texts.about.text}
+    </section>
+    <section class="features">
+        <h2 class="features__title">${texts.features.title}</h2>
+        <div class="features__grid">
+            ${texts.features.cards.map((card) => (
+              `<div class="features__item">${card}</div>`
+            )).join('')}
+        </div>
+    </section>
+`;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './styles/style.scss';
 import texts from './assets/data/text-resourses.json';
 import headerLogo from './assets/images/logo.svg';
-
-const container = document.querySelector('.content');
+import { getHome } from './components/home/home';
 
 function getHeader () {
   const menu = getMenu();
@@ -25,12 +24,17 @@ function getMenu () { // todo make active page without link
 </nav>`;
 }
 
-function getMain () {
-  return `<main class="main"></main>`;
+function getMain (pageContent = '') {
+  return `<main class="main">${pageContent}</main>`;
 }
 
 function getFooter () {
   return `<footer class="footer"><div class="footer__made-by">${texts.footer.madeBy}</div></footer>`;
 }
 
-container.innerHTML = getHeader() + getMain() + getFooter();
+function renderPage (componentsArray) {
+  const container = document.querySelector('.content');
+  container.innerHTML = componentsArray.join('');
+}
+
+renderPage([getHeader(), getMain(getHome()), getFooter()]);
