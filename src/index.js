72a5fc7ff71b0d3d60e46da7f70707deb2878449
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-11 08:10:48 +0300

    Add empty home page from js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './styles/style.scss';
+import texts from './assets/data/text-resourses.json';
+import headerLogo from './assets/images/logo.svg';
 
-// temp
-import heroImg from './assets/images/hero.webp';
-import logoImg from './assets/images/logo.svg';
-document.querySelector('.hero__image').src = heroImg;
-document.querySelector('.header__logo-img').src = logoImg;
+const container = document.querySelector('.content');
+
+function getHeader () {
+  const menu = getMenu();
+  return `<header class="header">
+                        <div class="wrapper">
+                            <div class="header__logo">
+                                <img src="${headerLogo}" alt="${texts.header.logoAltText}" class="header__logo-img">
+                                <h1 class="header__title">${texts.header.name}</h1>
+                            </div>
+                            ${menu || ''}
+                        </div>
+                    </header>`;
+}
+
+function getMenu () { // todo make active page without link
+  return `<nav class="menu">
+    <ul class="menu__list">${texts.menu.map((item) => (
+      `<li class="menu__item"><a href="${item.href}" class="menu__link">${item.name}</a></li>`
+    )).join('')}</ul>
+</nav>`;
+}
+
+function getMain () {
+  return `<main class="main"></main>`;
+}
+
+function getFooter () {
+  return `<footer class="footer"><div class="footer__made-by">${texts.footer.madeBy}</div></footer>`;
+}
+
+container.innerHTML = getHeader() + getMain() + getFooter();
