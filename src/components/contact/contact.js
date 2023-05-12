3e4677cb892c5783689607868ffda080b9975108
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-12 08:34:27 +0300

    Add address string and place text values into json

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,7 +4,8 @@ import texts from '../../assets/data/text-resourses.json';
 export function getContacts () {
   return `<section class="contacts">
             <div class="contacts__container">
-                <h2 class="contacts__title">Contact us</h2>
+                <h2 class="contacts__title">${texts.contacts.title}</h2>
+                <address class="contacts__address">${texts.contacts.address}</address>
                 <div class="contacts__map" id="map"></div>
             </div>
           </section>`;
