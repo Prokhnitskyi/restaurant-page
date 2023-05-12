f32d4837017f5c9e50e3ff08f95bb1dd41c96224
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-12 08:27:21 +0300

    Add leaflet map

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,7 +5,7 @@ export function getContacts () {
   return `<section class="contacts">
             <div class="contacts__container">
                 <h2 class="contacts__title">Contact us</h2>
-                <div class="contacts__map"></div>
+                <div class="contacts__map" id="map"></div>
             </div>
           </section>`;
 }
